bfe3dfe395afa9af0a29464420f1c26d870b5690
Maxim Grebennikov <m.grebennikov@awatera.com>
2024-12-01 14:59:28 +0300

    test

--- plugin.js
+++ plugin.js
@@ -1,13 +1,31 @@
+// Boosty Plugin for LAMPA
 (function () {
-  'use strict';
+  let BoostyPlugin = {
+    init: function () {
+      console.log('Boosty plugin initialized');
+      Lampa.Component.add('boosty', BoostyPlugin.createComponent());
+      BoostyPlugin.addMenuItem();
+    },
+    createComponent: function () {
+      return {
+        name: 'boosty',
+        create: function () {
+          this.html(`<div class="boosty-container">
+                        <h1>Добро пожаловать в Boosty</h1>
+                        <p>Здесь будет отображаться ваш контент.</p>
+                    </div>`);
+        }
+      };
+    },
+    addMenuItem: function () {
+      Lampa.Listener.add('menu', function (e) {
+        e.add({
+          title: 'Boosty',
+          component: 'boosty',
+        });
+      });
+    }
+  };
 
-  function startPlugin() {
-    Lampa.Noty.show('Hello');
-    console.log('Boosty plugin initialized');
-  }
-
-  if (!window.boosty_plugin_ready) {
-    window.boosty_plugin_ready = true;
-    startPlugin();
-  }
+  BoostyPlugin.init();
 })();
