cba16064baba7b418469e7c9903db8fcfcd0c849
Maxim Grebennikov <m.grebennikov@awatera.com>
2024-12-01 14:06:38 +0300

    test

---
+++ plugin.js
@@ -0,0 +1,30 @@
+(function () {
+  let BoostyPlugin = {
+    init: function () {
+      console.log('Boosty plugin initialized');
+      Lampa.Component.add('boosty', BoostyPlugin.createComponent());
+      BoostyPlugin.addMenuItem();
+    },
+    createComponent: function () {
+      return {
+        name: 'boosty',
+        create: function () {
+          this.html(`<div class="boosty-container">
+                        <h1>Добро пожаловать в Boosty</h1>
+                        <p>Здесь будет отображаться ваш контент.</p>
+                    </div>`);
+        }
+      };
+    },
+    addMenuItem: function () {
+      Lampa.Listener.add('menu', function (e) {
+        e.add({
+          title: 'Boosty',
+          component: 'boosty',
+        });
+      });
+    }
+  };
+
+  BoostyPlugin.init();
+})();
